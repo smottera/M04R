37b8e8f496928873a86b7e9bd7c567d29a44bf1c
Sam Mottera <samanthms131211@gmail.com>
2018-10-12 12:25:58 -0400

    Update linuxStartup.sh

--- linuxStartup.sh
+++ linuxStartup.sh
@@ -1,5 +1,18 @@
 sudo apt-get update
 sudo apt-get upgrade
 
-sudo apt install jre
+sudo apt-get install vim
+sudo apt-get install htop
 
+
+sudo apt install default-jdk
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install software-properties-common
+
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get install oracle-java9-installer
+sudo apt-get install oracle-java8-installer
+sudo apt-get update
+sudo apt-get upgrade
+wget -c --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/9.0.4+11/c2514751926b4512b076cc82f959763f/jdk-9.0.4_linux-x64_bin.tar.gz
